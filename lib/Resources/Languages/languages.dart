437ea73a7473fa0c77e6271998d8a1696388cb16
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-06-12 16:20:01 +0500

    1.Making Mvvm Architecture

--- lib/Resources/Languages/languages.dart
+++ lib/Resources/Languages/languages.dart
@@ -3,7 +3,6 @@ import 'package:get/get.dart';
 class Languages extends Translations{
 
   @override
-  // TODO: implement keys
   Map<String, Map<String, String>> get keys =>{
     'en_US':{
       'email_hint':'Enter Email'
