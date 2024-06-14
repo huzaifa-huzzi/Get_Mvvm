1270e72e79a9fc333dc94c59522eec26d16487ae
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-06-14 16:26:08 +0500

    1.Making Mvvm Architecture

--- lib/view/Home/HomeScreen.dart
+++ lib/view/Home/HomeScreen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx_mvvm/Resources/Routes/Routes_name.dart';
+import 'package:getx_mvvm/Utils/Utils.dart';
+import 'package:getx_mvvm/view_model/user_prefrences/user_prefrences.dart';
 
 
 
@@ -10,8 +14,23 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  UserPrefrences userPrefrences = UserPrefrences();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(onPressed: (){
+            userPrefrences.removeUser().then((value){
+              Get.toNamed(RouteName.loginView);
+            }).onError((error,stackTrace){
+              Utils.toastMessage(error.toString());
+            });
+          }, icon:const Icon(Icons.logout) )
+        ],
+      ),
+    );
   }
 }
--- lib/view_model/Services/splash_scervices.dart
+++ lib/view_model/Services/splash_scervices.dart
@@ -1,12 +1,24 @@
 import 'dart:async';
 import 'package:get/get.dart';
 import 'package:getx_mvvm/Resources/Routes/Routes_name.dart';
+import 'package:getx_mvvm/view_model/user_prefrences/user_prefrences.dart';
 
 class SplashServices {
 
+  UserPrefrences userPrefrences = UserPrefrences();
+
   void isLogin(){
+
+    userPrefrences.getUser().then((value){
+       if(value.token!.isEmpty || value.token.toString() == 'null' ){
+         Timer(const Duration(seconds: 3),() => Get.toNamed(RouteName.loginView));
+       }else{
+         Timer(const Duration(seconds: 3),() => Get.toNamed(RouteName.homeScreen));
+       }
+
+    });
   
-    Timer(const Duration(seconds: 3),() => Get.toNamed(RouteName.loginView));
+
 
   }
 
