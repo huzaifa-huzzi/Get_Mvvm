7316b6bfc67dd208402efecd133cf376e529dea3
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-06-14 17:01:59 +0500

    1.Making Mvvm Architecture

--- lib/view_model/Controller/Home/UserList_view_model.dart
+++ lib/view_model/Controller/Home/UserList_view_model.dart
@@ -13,14 +13,14 @@ class UserListViewModel extends GetxController{
 final RxrequestStatus = Status.Loading.obs;
 
  void setLoading(Status _value) => RxrequestStatus.value = _value;
-  void setUserList(UserListViewModel _value) => userList.value = _value;
+  void setUserList(UserListModel _value) => userList.value = _value as UserListViewModel;
 
   void userListApi(){
     api.userListApi().then((value){
       setLoading(Status.Completed);
       setUserList(value);
     }).onError((error,stackTrace){
-
+      setLoading(Status.Error);
     });
   }
 
